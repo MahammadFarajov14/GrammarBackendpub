d1b903fa868e13df4cf597360c0893dc53be740b
Mahammad Farajov14 <mahammadfarajov1029876543@gmail.com>
2025-06-11 18:27:50 +0400

    changes

--- grammar/settings.py
+++ grammar/settings.py
@@ -49,17 +49,24 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'drf_yasg',
     'django_filters',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'grammar-azi.vercel.app',
+    'https://localhost:5173',
 ]
 
 ROOT_URLCONF = 'grammar.urls'
