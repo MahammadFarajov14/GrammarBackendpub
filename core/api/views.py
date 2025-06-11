9749a4ea6e096549c0d3d8d9836edfeea69bf3a5
Mahammad Farajov14 <mahammadfarajov1029876543@gmail.com>
2025-06-11 19:20:01 +0400

    changes

--- core/api/views.py
+++ core/api/views.py
@@ -29,7 +29,9 @@ class CheckUpApiView(ListCreateAPIView):
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=self.request.data)
         if serializer.is_valid():
-            checkup = serializer.save(commit=False)
+            validated_data = serializer.validated_data
+            checkup = Checkup.objects.create(**validated_data)
+            # Optionally modify or inspect validated_data
             message = f'comment:{checkup.comment},\nphone_number:{checkup.phone_number}'
             file = f'file:{checkup.file.file}'
             url = f'https://api.telegram.org/bot{my_token}/sendMessage?chat_id={my_chatid}&text={message}'
